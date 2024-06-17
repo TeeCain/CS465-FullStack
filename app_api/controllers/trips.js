74547ed40c827160da5263854f2a6a832f0dbdda
TeeCain <111044931+TeeCain@users.noreply.github.com>
2024-06-17 14:20:56 -0400

    Module 6 baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -4,7 +4,7 @@ const Trip = require('../models/travlr');
 const Model = mongoose.model('trips');
 
 // GET: /trips - lists all the trips
-// Regardless of outcome, response must include HTML status code and JSON message to the rewuesting client
+// Regardless of outcome, response must include HTML status code and JSON message to the requesting client
 const tripsList = async(req, res) => {
     const q = await Model
         .find({}) // No filter, return all records
@@ -44,7 +44,33 @@ const tripsFindByCode = async (req, res) => {
     }
   };
 
+  // POST: /trips- Adds a new Trip
+  // Regardless of outcome, response must include HTML status code and JSON message to the requesting client
+const tripsAddTrip= async(req, res) => {
+  const newTrip= new Trip({
+  code: req.body.code,
+  name: req.body.name,
+  length: req.body.length,
+  start: req.body.start,
+  resort: req.body.resort,
+  perPerson: req.body.perPerson,
+  image: req.body.image,
+  description: req.body.description
+  });
+
+  const q = await newTrip.save();
+  if (!q)
+    { // Database returned no data
+      return res.status(400).json(err);
+    }else 
+    { // Return new trip
+      return res.status(201).json(q);
+    }
+    
+}
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,16 +3,18 @@ const express = require('express');
 // Router logic
 const router = express.Router();
 
-// This is where we import the controllers we will route
+// Import the controllers we will route
 const tripsController = require('../controllers/trips');
 
 // Define route for our trips endpoint
 router  
     .route('/trips')
-    .get(tripsController.tripsList); // GET Method routes tripsList
+    .get(tripsController.tripsList) // GET Method routes tripsList
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
  
+// GET Method routes tripsFindByCode - requires parameter
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode); // GET Method routes tripsFindByCode - requires parameter
+    .get(tripsController.tripsFindByCode); 
 
 module.exports = router;
